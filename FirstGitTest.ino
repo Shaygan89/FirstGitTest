aca34fbade60ceb3ed27a67dfc0f4987174d7db4
Shaygan <sh823333@gmail.com>
2025-09-04 19:41:25 +0330

    Final version of this test code

--- FirstGitTest.ino
+++ FirstGitTest.ino
@@ -2,14 +2,14 @@
 #include <LiquidCrystal_PCF8574.h>
 
 // Current firmware version
-String currentVersion = "2.0.0";
+String currentVersion = "2.2.2";
 
 // ---------------- LCD ----------------
 LiquidCrystal_PCF8574 lcd(0x27); // I2C address
 
 void setup() {
   // put your setup code here, to run once:
-    // LCD initgit 
+  //LCD.init
   lcd.begin(20, 4);
   lcd.setBacklight(255);
   lcd.setCursor(0,0);
@@ -25,6 +25,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   lcd.setCursor(0,0);
   lcd.clear();
-  lcd.print("This is soo cool");
+  lcd.print("This is soooo cool");
+  lcd.setCursor(0,1);
+  lcd.print("and finall version(:");
 
 }
